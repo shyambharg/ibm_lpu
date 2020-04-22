56d4a4f834d7f8b6d3880c12eb5cba96752dcd99
shyam <shyam@192.168.1.5>
2020-04-22 15:18:01 +0530

    commit

--- CRMRest/src/main/java/com/example/service/CustomerServiceImplement.java
+++ CRMRest/src/main/java/com/example/service/CustomerServiceImplement.java
@@ -2,10 +2,10 @@ package com.example.service;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.dao.CustomerDao;
 import com.example.entity.Customer;
