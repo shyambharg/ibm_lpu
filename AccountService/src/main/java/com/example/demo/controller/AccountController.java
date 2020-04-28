05fdca4a0efe794ac84a360a570ffd6f30539546
shyam <shyam@192.168.1.5>
2020-04-28 15:34:06 +0530

    commit

--- AccountService/src/main/java/com/example/demo/controller/AccountController.java
+++ AccountService/src/main/java/com/example/demo/controller/AccountController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Account;
 import com.example.demo.service.AccountService;
 
+@RestController
 public class AccountController {
 	
 	@Autowired
